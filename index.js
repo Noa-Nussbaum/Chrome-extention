4209d3595aa486d05be71e67f060d01e9e27172e
Noa <noa.nussb@gmail.com>
2025-05-21 11:36:50 +0300

    Removed tab button and added total

--- index.js
+++ index.js
@@ -1,28 +1,86 @@
 // Get values
 const saveBtn = document.getElementById("expense-btn")
-const expense = document.getElementById("expense-el")
-const amount = document.getElementById("amount-el")
+const expenseEl = document.getElementById("expense-el")
+const amountEl = document.getElementById("amount-el")
 const expenseUlEl = document.getElementById("expense-ul-el")
-const amountUlEl = document.getElementById("amount-ul-el")
+const deleteBtn = document.getElementById("delete-btn")
 
-// Set empty lists
 let expenseList = []
-let amountList = []
+
+const totalEl = document.getElementById("total-el")
+let total = 0
+
+// Get existing list / start new one
+const expenseListStorage = JSON.parse(localStorage.getItem("MyExpenses"));
+
+if(Array.isArray(expenseListStorage) && expenseListStorage.every(e => e !== null)){
+    expenseList = expenseListStorage
+    render(expenseList)
+}
+
+
+// Render list
+function render(leads) {
+    // Clear existing list
+    expenseUlEl.innerHTML = "";
+
+    leads.forEach((item) => {
+        const li = document.createElement("li");
+        li.textContent = `${item.name} : $${item.value}`;
+        expenseUlEl.appendChild(li);
+    });
+
+    totalEl.innerHTML = `TOTAL: $${total}`
+    // totalEl.innerHTML = "hello"
+    console.log(total)
+}
+
+// Delete all we have so far
+deleteBtn.addEventListener("dblclick", function(){
+
+    localStorage.clear()
+    expenseList = []
+    render(expenseList)
+
+    expenseEl.value = ""
+    amountEl.value = ""
+    // window.resizeTo(document.body.scrollWidth, document.body.scrollHeight);
+
+    total = 0
+
+})
 
 // Save the input
 saveBtn.addEventListener("click", function(){
-    console.log("Expense saved successfully!");
+    // Retrieve value
+    let expense = expenseEl.value
+    let amount = parseFloat(amountEl.value) || 0
 
-    expenseList.push(`<li>
-        <a target ='_blank' href=' ${expense.value} '>
-        ${expense.value} 
-        </a>
-        </li>`)
-    amountList.push(amount.value)
+    if(expense && amount){
+        const item = {
+            name: expense,
+            value: parseFloat(amount)
+        };
 
-    expense.value = "Expense location"
-    amount.value = 0
+        expenseList.push(item)
+        localStorage.setItem("MyExpenses", JSON.stringify(expenseList))
+        // Render function
+        total += amount
+        render(expenseList)
 
-    expenseUlEl.innerHTML = expenseList
-    amountUlEl.innerHTML = amountList
+        console.log("Expense saved successfully!");
+
+        
+    }    
+    // Restart default background values
+    expenseEl.value = ""
+    amountEl.value = ""
+
+    console.log(expenseList)
+  
 })
+
+
+
+
+
